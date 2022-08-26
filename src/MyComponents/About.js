97b54e98164e385c0a8a64260fc0fa2dcbebb2a1
Himanshu <himanshuyadav9536w@gmail.com>
2022-08-27 00:16:07 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component for a project made by using React in which we can add some task with description and we can delete them.
+        </div>
+    )
+}
